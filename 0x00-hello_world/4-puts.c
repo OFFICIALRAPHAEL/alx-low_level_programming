09dcb9db10cd1ad502420a0ed7162b939cda8123
OFFICIAL_RAPHAEL <officialraphael@gmail.com>
2022-09-09 03:43:25 +0300

    Low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,10 +7,6 @@
  */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
-
-	puts(str);
-
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
