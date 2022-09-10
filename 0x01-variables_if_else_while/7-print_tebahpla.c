b27faf23c117c67f65bf47673fdf6df19274f60e
OFFICIAL_RAPHAEL <officialraphael@gmail.com>
2022-09-10 03:40:52 +0300

    Functions

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main (void)
+{
+       	char ch;
+	for(ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+       	return(0);
+}
